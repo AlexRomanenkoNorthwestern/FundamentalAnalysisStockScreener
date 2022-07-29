9a27cce9411bda0099aa45d2096f49406a249689
AlexRomanenkoNorthwestern <99095505+AlexRomanenkoNorthwestern@users.noreply.github.com>
2022-07-29 12:28:26 -0400

    Update General.py

--- General.py
+++ General.py
@@ -5,20 +5,12 @@ import pandas as pd
 import pytrends
 from pytrends.request import TrendReq
 pytrends = TrendReq()
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta, date, time
 import math
 import plotly.graph_objects as go
 import finnhub
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup, element
-import os
 import matplotlib.pyplot as plt
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import numpy as np
-import time
 
 from dateutil.relativedelta import relativedelta
 import yfinance as yf
@@ -27,15 +19,15 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 import math
 import plotly.express as px
-import matplotlib.pyplot as plt
 
-text_data = open("APIKey.txt", "r")
-apiKey = text_data.readline()
-apiKey = apiKey[0:len(apiKey)-1]
-text_data.close()
+
+apiKey = "cb7kvuiad3i5ufvorpb0"
 
 # API Keys
 finnhub_client = finnhub.Client(api_key=apiKey)
+
+def get_finnhub_client():
+    return(finnhub_client)
 
 # Retrieves a stock ticker's company name and removes business types such as LLC, Ltd, Inc
 # Example Usage: get_brand_name('CROX')
@@ -43,8 +35,8 @@ def get_brand_name(ticker):
     ticker = str(ticker)
     try:
         company_name = finnhub_client.company_profile2(symbol= ticker)['name'].split(' ')
-    except:
-        company_name = ticker
         company_name = company_name[0:len(company_name)-1]
         company_name = ' '.join(company_name)
+    except:
+        company_name = ticker
     return(company_name)
