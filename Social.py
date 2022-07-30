7b4d83d7d2354963e553eb4724186ae944a08275
AlexRomanenkoNorthwestern <99095505+AlexRomanenkoNorthwestern@users.noreply.github.com>
2022-07-30 03:22:15 -0400

    Update Social.py

--- Social.py
+++ Social.py
@@ -8,6 +8,8 @@ import snscrape.modules.twitter as sntwitter
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
    
 def social_volume_reddit(ticker):
     today = datetime.now().strftime("%Y-%m-%d")
@@ -59,3 +61,42 @@ def relative_search_volume_twitter (ticker):
         return ("+{}%".format(int(result)))
     else:
         return("{}%".format(int(result)))
+
+def tweets_volume_figure(ticker, total_days):
+    start_date =  datetime.today().date() - timedelta(days=total_days -1)
+    end_date = datetime.today().date()
+    delta = timedelta(days=1)
+    volume_list = []
+    count = 0
+    total = 0
+    while start_date <= end_date:
+        volume = len(list(sntwitter.TwitterSearchScraper(
+                '${} since:{} until:{}'.format(ticker, start_date, start_date + delta)).get_items()))
+        if (count < 7):
+            count += 1
+            total += volume
+            volume_list.append([start_date, volume, total/count ])
+
+        else:
+            total = 0
+            for i in range(6):
+                total += volume_list[len(volume_list) - i -1][1]
+
+            volume_list.append([start_date, volume, (total+volume)/7 ])
+        start_date += delta
+
+
+    tweets_df = pd.DataFrame(volume_list, columns=['Date', 'Tweet Volume', "7 Day Moving Average"])
+    fig = px.bar(tweets_df, x = "Date", y = "Tweet Volume")
+    fig.add_trace(go.Scatter(x = tweets_df['Date'], y = tweets_df["7 Day Moving Average"],
+                    mode='lines+markers',
+                    name='7 Day SMA',
+                    marker_color = '#00CC96'))
+    fig.update_layout(showlegend= False)
+    fig.update_layout(margin=dict(l=20, r=20, t=30, b=20),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font_color = 'white')
+    fig.update_xaxes(tickangle = 0)
+    return(fig)
+
