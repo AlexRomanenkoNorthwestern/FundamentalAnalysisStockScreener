ac19de4b4339e59be2b9416133de162cf56420d2
AlexRomanenkoNorthwestern <99095505+AlexRomanenkoNorthwestern@users.noreply.github.com>
2022-07-31 04:14:10 -0400

    Update Finviz.py

--- Finviz.py
+++ Finviz.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup, element
 import pandas as pd
 from datetime import datetime, timedelta, date, time
 import finnhub
+import time
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 # Used for a table
@@ -41,7 +44,7 @@ def net_insider_transactions(ticker):
     try:
         df = General.get_finnhub_client().stock_insider_transactions(ticker, three_months_ago, today)['data']
     except:
-        time.sleep(10)
+        time.sleep(3)
         df = General.get_finnhub_client().stock_insider_transactions(ticker, three_months_ago, today)['data']
 
     net = 0
@@ -54,3 +57,28 @@ def net_insider_transactions(ticker):
         return ("+{}%".format(result))
     else:
         return("{}%".format(result))
+
+def insider_chart(ticker):
+    today = datetime.now().strftime("%Y-%m-%d")
+    three_months_ago = (datetime.now() + relativedelta(months=-3)).strftime("%Y-%m-%d")
+    try:
+        df = General.get_finnhub_client().stock_insider_transactions(ticker, three_months_ago, today)['data']
+    except:
+        time.sleep(3)
+        df = General.get_finnhub_client().stock_insider_transactions(ticker, three_months_ago, today)['data']
+
+    positive_sum = 0
+    negative_sum = 0
+    for i in range(len(df)):
+        if ((int(df[i]['change'])) > 0):
+            positive_sum += int(df[i]['change'])
+        else:
+            negative_sum += abs(int(df[i]['change']))
+    
+    fig = px.pie(values = [positive_sum, negative_sum], names = ["Positive", "Negative"], hole = 0.5)
+    fig.update_layout(margin=dict(l=20, r=20, t=30, b=20),
+                          paper_bgcolor='rgba(0,0,0,0)',
+                          plot_bgcolor='rgba(0,0,0,0)',
+                          font_color = 'white')
+    return(fig)
+
